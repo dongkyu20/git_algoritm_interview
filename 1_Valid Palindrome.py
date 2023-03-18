4db2899ce94dd69d7e7cf3b8b157c94d049d0e6e
dongkyu20 <hankobe2000@donga.ac.kr>
2023-03-18 20:12:07 +0900

    1번 내 풀이

--- 1_Valid Palindrome.py
+++ 1_Valid Palindrome.py
@@ -1,25 +1,26 @@
-# 내 풀이
-# isalnum, lower 참고
-def isPalindrome(self, s:str) -> bool:
+    # 내 풀이
+    # isalnum, lower 참고
 
-    # 영어와 숫자만 입력
-    strs = []
-    for char in s:
-        if char.isalnum():
-            strs.append(char.lower())
+class Solution:
+    def isPalindrome(self, s:str) -> bool:
 
-    # 팰린드롬
-    last = len(strs) - 1
-    first = 0
-    while first <= last:
-        if strs[first] == strs[last]:
-            first += 1
-            last -= 1
-        else:
-            return False
-    if first > last:
-        return True
+        # 영어와 숫자만 입력
+        strs = []
+        for char in s:
+            if char.isalnum():
+                strs.append(char.lower())
 
+        # 팰린드롬
+        last = len(strs) - 1
+        first = 0
+        while first <= last:
+            if strs[first] == strs[last]:
+                first += 1
+                last -= 1
+            else:
+                return False
+        if first > last:
+            return True
 
 
 
