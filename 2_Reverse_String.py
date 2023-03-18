b7b7b61bb149a81e25b34f3dfc366fb9cd8dfe7b
dongkyu20 <79791066+dongkyu20@users.noreply.github.com>
2023-03-18 20:11:47 +0900

    Delete 2_Reverse_String.py
    계정변경

--- 2_Reverse_String.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    # reverse 활용
-    # def reverseString(self, s: list[str]) -> None:
-    #     s.reverse()
-    def reverseString(self,s : list[str])-> None:
-        a = []
-        for i in range(len(s)):
-            a.append(s[len(s)-1-i])
-        s = a
