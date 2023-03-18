ba26d0969d1ed1143bc547dfd8feaaced293a5bc
dongkyu20 <hankobe2000@gmail.com>
2023-03-18 21:27:30 +0900

    2번 내 풀이 수정

--- 2_Reverse String.py
+++ 2_Reverse String.py
@@ -1,11 +1,19 @@
+s = ["H","a","n","n","a","h"]
 
-
-# 내 풀이 리트에서는 틀렸다고 나옴
 def reverseString(self, s:list[str]) -> None:
-    a = []
-    for i in range(len(s)):
-        a.append(s[len(s)-1-i])
+    left = 0
+    right = len(s)-1
 
-    s = a
+    while left < right:
+        s[left], s[right] = s[right] , s[left]
+        left += 1
+        right -= 1
 
-# 포인터나 reverse 메서드 사용
+
+# 내 풀이 리트에서는 틀렸다고 나옴 --> !!!do not allocate another array!!!
+# def reverseString(self, s:list[str]) -> None:
+#     a = []
+#     for i in range(len(s)):
+#         a.append(s[len(s)-1-i])
+#     s = a
+
